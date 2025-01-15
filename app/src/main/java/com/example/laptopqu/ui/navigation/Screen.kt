f12631f86058f75bfdd4039e024b8f9eb7975e4c
daniharmade <harmadedani@gmail.com>
2025-01-15 13:39:41 +0700

    FIX ACC

--- app/src/main/java/com/example/laptopqu/ui/navigation/Screen.kt
+++ app/src/main/java/com/example/laptopqu/ui/navigation/Screen.kt
@@ -3,7 +3,7 @@ package com.example.laptopqu.ui.navigation
 sealed class Screen(val route: String) {
     object Home : Screen("home")
     object Favorite : Screen("favorite")
-    object Profile : Screen("profile")
+    object Profile : Screen("about_page")
     object DetailLaptop : Screen("home/{laptopId}") {
         fun createRoute(laptopId: Int) = "home/$laptopId"
     }
